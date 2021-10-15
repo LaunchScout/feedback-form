f41c9ceb500cf16ab843841d41ac061e8d4c57d2
Chris Nelson <chris@gaslight.co>
2021-10-14 22:56:53 -0400

    clean up a lil

--- src/FeedbackForm.ts
+++ src/FeedbackForm.ts
@@ -18,17 +18,20 @@ export class FeedbackForm extends LitElement {
     }
   `;
 
-  async submitFeedback(event: Event) {
-    event.preventDefault()
-    const formData = new FormData(event.target as HTMLFormElement)
+  formDataAsJSON(form: HTMLFormElement) {
+    const formData = new FormData(form)
     const data: { [k: string]: any } = {};
     formData.forEach((value, key) => (data[key] = value))
+    return JSON.stringify(data);
+  }
+
+  async submitFeedback(event: Event) {
+    event.preventDefault()
     const response = await fetch(this.url, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(data)
+      body: this.formDataAsJSON(event.target as HTMLFormElement)
     });
-    console.log(response);
     this.displayForm = false;
     this.displaySuccess = true;
   }
@@ -36,28 +39,26 @@ export class FeedbackForm extends LitElement {
   render() {
     return html`
       ${this.displayForm ? html`
-        <slot name="form">
-          <form @submit=${this.submitFeedback} part="form">
-            <div part="name-input-group">
-              <label>Name</label>
-              <input name="name" part="input"></input>
-            </div>
-            <div part="email-input-group">
-              <label>Email</label>
-              <input name="email" part="input"></input>
-            </div>
-            <div part="textarea-input-group">
-              <label>Message</label>
-              <textarea name="message" part="input"></textarea>
-            </div>
-            <button part="submit-button">Send Feedback</button>
-          </form>
-        </slot>
+        <form @submit=${this.submitFeedback} part="form">
+          <div part="input-group">
+            <label>Name</label>
+            <input name="name" part="input"></input>
+          </div>
+          <div part="input-group">
+            <label>Email</label>
+            <input name="email" part="input"></input>
+          </div>
+          <div part="input-group">
+            <label>Message</label>
+            <textarea name="message" part="input"></textarea>
+          </div>
+          <button part="submit-button">Send Feedback</button>
+        </form>
       ` : html`
         <button @click=${this.openFeedback} part="open-feedback-button" slot="open-feedback">Feedback!</button>
       `}
       ${this.displaySuccess ? html`
-        <slot name="success"><h1>Great job!</h1></slot>
+        <slot name="success"><h1>You submitted some feedback!! Great job!!</h1></slot>
       ` : ''}
     `;
   }
--- test/feedback-form.test.ts
+++
@@ -1,8 +0,0 @@
-import { html } from 'lit';
-import { fixture, expect } from '@open-wc/testing';
-import { FeedbackForm } from '../src/FeedbackForm.js';
-import '../src/feedback-form.js';
-
-describe('FeedbackForm', () => {
-
-});
